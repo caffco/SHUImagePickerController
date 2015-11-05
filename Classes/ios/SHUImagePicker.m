a3954a34a2da7cd86c095c189c9cfd2c417bf454
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2015-11-05 12:38:24 +0100

    Updated colors to match new color palette.

--- Classes/ios/SHUImagePicker.m
+++ Classes/ios/SHUImagePicker.m
@@ -53,12 +53,9 @@
     self.callbackBlock = callback;
     self.imagePickerController = [[UIImagePickerController alloc] init];
     self.imagePickerController.navigationBar.translucent = NO;
-    self.imagePickerController.navigationBar.barTintColor = [UIColor colorWithRed:
-            68.0 / 255.0
-            green:68.0 / 255.0
-            blue:68.0 / 255.0
-            alpha:1.0];
-    self.imagePickerController.navigationBar.tintColor = [UIColor whiteColor];
+    self.imagePickerController.navigationBar.barTintColor = [UIColor whiteColor];
+    self.imagePickerController.navigationBar.tintColor = [UIColor colorWithRed:0.894
+            green:0.106 blue:0.22 alpha:1];
     self.imagePickerController.navigationBar.barStyle = UIBarStyleBlack;
     self.imagePickerController.delegate = self;
     self.imagePickerController.sourceType = sourceType;
